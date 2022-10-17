45cf11debb29aa1d6f899debf54d1ab9c3d9c398
DylanLale <disneygamer706@gmail.com>
2022-10-17 09:38:53 -0400

    1.1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int intlist;
             string choice = "";
+            int addlist;
             Random generator = new Random();
-            intlist = (generator.Next(10, 21)); Console.Write(generator.Next(10, 21));
-            List<int> numbers = new List<int>();
+            List<double> numbers = new List<double>();
+            for(int i = 0; i < 25; i++) 
+                numbers.Add(generator.Next(10, 21));
+
+            List<double> numbers2 = new List<double>();
+            for (int i = 0; i < 25; i++)
+                numbers.Add(generator.Next(10, 21));
+
             while (choice != "8")
             {
-                Console.Clear(); // Optional
+                for(int i = 0; i < 25; i++) 
+                    Console.Write($"{numbers[i]},");
+                Console.WriteLine();
                 Console.WriteLine("Welcome to my list menu.  Please select an option:");
                 Console.WriteLine();
                 Console.WriteLine("1 - Sort List");
@@ -29,62 +37,62 @@
 
                 if (choice == "1")
                 {
-                    //Do option 1
+           
                     Console.WriteLine("You chose option 1");
+                    Console.WriteLine(numbers.Sort());
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
                 else if (choice == "2")
                 {
-                    // Do option 2
+            
                     Console.WriteLine("You chose option 2");
+                    for (int i = 0; i < 25; i++)
+                        Console.Write($"{numbers2[i]},");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
                 else if (choice == "3")
                 {
-                    // Do option 2
+                  
                     Console.WriteLine("You chose option 3");
+                    Console.WriteLine("Enter a number to remove from the list");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
 
                 }
                 else if (choice == "4")
                 {
-                    // Do option 2
+                    
                     Console.WriteLine("You chose option 4");
-                    Console.WriteLine("Hit ENTER to continue.");
-                    Console.ReadLine();
-                }
-                else if (choice == "2")
-                {
-                    // Do option 2
-                    Console.WriteLine("You chose option 2");
+                    Console.WriteLine("Add a number to the list");
+                    addlist = Convert.ToInt32(Console.ReadLine());
+                    numbers.Add(addlist);
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
                 else if (choice == "5")
                 {
-                    // Do option 2
+                   
                     Console.WriteLine("You chose option 5");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
                 else if (choice == "6")
                 {
-                    // Do option 2
+               
                     Console.WriteLine("You chose option 6");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
                 else if (choice == "7")
                 {
-                    // Do option 2
+                
                     Console.WriteLine("You chose option 7");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                 }
-                // Add an else if for each valid choice...
+               
                 else
                 {
                     Console.WriteLine("Invalid choice, press ENTER to continue.");
